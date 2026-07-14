2d51c233673bbd1877d4070a97445f0b2144ef2a
leohornersz <leohornersz@gmail.com>
2026-07-14 07:07:02 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'herramientas-psl-v9';
+var CACHE_NAME = 'herramientas-psl-v10';
 var URLS_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(event) { event.waitUntil(caches.open(CACHE_NAME).then(function(cache) { return cache.addAll(URLS_TO_CACHE); })); self.skipWaiting(); });
 self.addEventListener('activate', function(event) { event.waitUntil(caches.keys().then(function(names) { return Promise.all(names.filter(function(name) { return name !== CACHE_NAME; }).map(function(name) { return caches.delete(name); })); })); self.clients.claim(); });
